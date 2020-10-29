2d818a328c0978b6c8fd9da8c6073339c4b67641
henriB <henri.bretseau@gmail.com>
2020-10-29 15:11:05 +0100

    const general

---
+++ script.js
@@ -0,0 +1,5 @@
+const canvas = document.getElementById('canvas');
+const score = document.getElementById('score');
+const days = document.getElementById('days');
+const endScreen = document.getElementById('endScreen');
+
